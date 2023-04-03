ae69c97f8ea583fd3a09ea57495d2855a1932147
kxx <132kxx@gmail.com>
2023-04-03 09:01:29 +0900

    Initial Commit

---
+++ Today/TodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodayApp.swift
+//  Today
+//
+//  Created by kxx: on 2023/04/03.
+//
+
+import SwiftUI
+
+@main
+struct TodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
